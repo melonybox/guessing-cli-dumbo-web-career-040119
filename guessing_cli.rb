74da4ecf08876c5dcb5e0974b073c0cfce518ae6
Gino Desangles <blushingbox@yahoo.com>
2019-03-24 18:55:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,7 +9,7 @@ def run_guessing_game
     if "#{theInput}" == 'exit'
       puts "Goodbye!"
       break
-    elsif theInput.to_i == numA
+    elsif "#{theInput}" == numA
       puts "You gussed the correct number!"
     else
       puts "The computer gussed #{numA}."
